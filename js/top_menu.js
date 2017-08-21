cd4cd7e7b092e2891a6cef759b84bdf84307cd66
nerarosa <tienhv0502@gmail.com>
2017-08-21 16:17:54 +0700

    a

---
+++ js/top_menu.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('#header-1').outerHeight() + 96;
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('#header-1').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('#header-1').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
+});
