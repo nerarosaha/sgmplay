04cfde8f64405c76beb38eb40dceac4d4afad7a2
nerarosa <tienhv0502@gmail.com>
2017-08-21 16:02:09 +0700

    1

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -96,7 +96,7 @@ var padding = 4;
 							
 							var change_alias = function(a){a=a.toLowerCase();a=a.replace(/\u00e0|\u00e1|\u1ea1|\u1ea3|\u00e3|\u00e2|\u1ea7|\u1ea5|\u1ead|\u1ea9|\u1eab|\u0103|\u1eb1|\u1eaf|\u1eb7|\u1eb3|\u1eb5/g,"a");a=a.replace(/\u00e8|\u00e9|\u1eb9|\u1ebb|\u1ebd|\u00ea|\u1ec1|\u1ebf|\u1ec7|\u1ec3|\u1ec5/g,"e");a=a.replace(/\u00ec|\u00ed|\u1ecb|\u1ec9|\u0129/g,"i");a=a.replace(/\u00f2|\u00f3|\u1ecd|\u1ecf|\u00f5|\u00f4|\u1ed3|\u1ed1|\u1ed9|\u1ed5|\u1ed7|\u01a1|\u1edd|\u1edb|\u1ee3|\u1edf|\u1ee1/g,"o");a=a.replace(/\u00f9|\u00fa|\u1ee5|\u1ee7|\u0169|\u01b0|\u1eeb|\u1ee9|\u1ef1|\u1eed|\u1eef/g,"u");a=a.replace(/\u1ef3|\u00fd|\u1ef5|\u1ef7|\u1ef9/g,"y");return a.replace(/\u0111/g,"d")}
 							
-                            (function searchData(url){
+                            function searchData(url){
 								if(url === undefined) url = opts.url;
 								console.log(url);
 								$.ajax({
@@ -215,7 +215,9 @@ var padding = 4;
 										}
 									}
 								})
-							})();
+							}
+							
+							searchData();
                         }, 0)
                     }
                 } else {
